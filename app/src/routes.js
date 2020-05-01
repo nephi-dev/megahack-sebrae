3823f7c4e6105f9a3d005d9fa6c15e75de06d8f6
Carlos Daniel Rigoni <59850567+danrigoni@users.noreply.github.com>
2020-05-01 17:38:34 -0300

    feat: navigation and routing

---
+++ app/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const AppStack = createStackNavigator();
+
+import HomeScreen from './pages/HomeScreen'
+import LoginScreen from './pages/LoginScreen'
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+
+                <AppStack.Screen name="Home" component={HomeScreen}/>
+                <AppStack.Screen name="Login" component={LoginScreen}/>
+
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+    
+}
