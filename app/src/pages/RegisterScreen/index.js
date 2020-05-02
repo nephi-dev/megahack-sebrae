e69366643a632b7bd7d3c321958461abb2d09aad
Carlos Daniel Rigoni <59850567+danrigoni@users.noreply.github.com>
2020-05-02 11:52:43 -0300

    interface register

--- app/src/pages/RegisterScreen/index.js
+++ app/src/pages/RegisterScreen/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
 
-// import { Container } from './styles';
+import { Container, Left, Title, Form, Input, BorderInput, RegisterButton, RegisterButtonText } from './styles';
 
 export default function RegisterScreen() {
   return (
-    <View />
+    <Container>
+      <Left>
+        <AntDesign name="left" size={17} color="#000"/>
+      </Left>
+
+      <Title>Cadastrar</Title>
+
+      <Form>
+        <Input 
+          placeholder='Nome:'/>
+        <BorderInput />
+        
+        <Input 
+          placeholder='Email:'/>
+        <BorderInput />
+
+        <Input 
+          placeholder='Senha:'/>
+        <BorderInput />
+      
+        <RegisterButton>
+          <RegisterButtonText>CADASTRAR</RegisterButtonText>
+        </RegisterButton>
+
+      </Form>
+
+    </Container>
   );
 }
