a432fbd6ac231ff7d215e4e6a1ea5eac50102869
Carlos Daniel Rigoni <59850567+danrigoni@users.noreply.github.com>
2020-05-03 20:38:08 -0300

    feat: interface profile

--- app/src/pages/ProfileScreen/index.js
+++ app/src/pages/ProfileScreen/index.js
@@ -1,10 +1,96 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
+import Image from '../../assets/profile.jpg'
+import { useNavigation } from '@react-navigation/native'
 
-// import { Container } from './styles';
+import { Container, Header, IconLeft, BorderInput, TitleHeader, ProfileBar, ProfileImage, ProfileInfosBar, ProfileInfosHorizontal, ProfileInfoIcon, ProfileInfosVertical, ProfileInfoTitle, ProfileInfoText, UpdateButton, UpdateButtonText, AddServicesButton, AddServicesButtonText, ServicesBar, ServiceTitle, ServicesItemsBar, ServicesItemsBarHorizontal, ServiceImage, ServicesItemsBarVertical, ServicesTitle, ServiceDescription } from './styles';
 
 export default function ProfileScreen() {
+  const navigation = useNavigation()
+
+  function navigateToHome() {
+    navigation.navigate('Home')
+  }
+
   return (
-    <View />
+    <Container>
+      <Header>
+        <IconLeft onPress={navigateToHome}>
+          <AntDesign name="left" size={20} color="#000"/>
+        </IconLeft>
+        <TitleHeader>Perfil</TitleHeader>
+      </Header>
+
+      <ProfileBar>
+        <TouchableOpacity>
+          <ProfileImage source={Image}/>
+        </TouchableOpacity>
+
+        <ProfileInfosBar>
+          <ProfileInfosHorizontal>
+
+            <ProfileInfoIcon>
+              <AntDesign name="user" size={20} color="#000"/>
+            </ProfileInfoIcon>
+
+              <ProfileInfosVertical>
+                <ProfileInfoTitle>Nome</ProfileInfoTitle>
+                <ProfileInfoText placeholder="Art&Cia"/>
+              </ProfileInfosVertical>
+
+          </ProfileInfosHorizontal>
+        </ProfileInfosBar>
+
+
+        <ProfileInfosBar>
+          <ProfileInfosHorizontal>
+
+            <ProfileInfoIcon>
+              <AntDesign name="user" size={20} color="#000"/>
+            </ProfileInfoIcon>
+
+              <ProfileInfosVertical>
+                <ProfileInfoTitle>Descricao</ProfileInfoTitle>
+                <ProfileInfoText placeholder="Empresa a 10 anos no mercado, Dancarinos profissionais, pintura corporal, tecnicas de danca"/>
+              </ProfileInfosVertical>
+
+          </ProfileInfosHorizontal>
+
+          <UpdateButton>
+            <UpdateButtonText>Atualizar Perfil</UpdateButtonText>
+          </UpdateButton>
+          <BorderInput />
+        </ProfileInfosBar>
+
+
+      </ProfileBar>
+
+      <AddServicesButton>
+        <AddServicesButtonText>Adicionar novo serviços</AddServicesButtonText>
+      </AddServicesButton>
+
+      <ServicesBar>
+        <ServicesTitle>Meus serviços</ServicesTitle>
+
+        <ServicesItemsBar
+        data={[1,2,3, 4, 5, 6, 7, 8, 9]}
+        keyExtractor={item => String(item)}
+        horizontal={true}
+        renderItem={( item ) => (
+
+          <ServicesItemsBarHorizontal>
+            <ServiceImage source={Image} />
+
+            <ServicesItemsBarVertical>
+              <ServiceTitle>Aula de violao</ServiceTitle>
+              {/* <ServiceDescription>Aula de violao ao vivo</ServiceDescription> */}
+            </ServicesItemsBarVertical>
+          </ServicesItemsBarHorizontal>
+
+        )}
+          />
+      </ServicesBar>
+    </Container>
   );
 }
---
+++ app/src/pages/ProfileScreen/styles.js
@@ -0,0 +1,136 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  
+`;
+
+export const Header = styled.View`
+  height: 60px;
+  background-color: #FFF246;
+  flex-direction: row;
+  padding-top: 30px;
+`;
+
+export const IconLeft = styled.TouchableOpacity`
+  margin-left: 4px;
+`;
+
+export const TitleHeader = styled.Text`
+  font-size: 20px;
+  margin-top: -4px;
+  margin-left: 7px;
+
+`;
+
+export const ProfileBar = styled.View`
+  margin: 5px;
+`;
+
+export const ProfileImage = styled.Image`
+  margin-top: 20px;
+  height: 150px;
+  width: 150px;
+  border-radius: 100px;
+  align-self: center;
+`;
+
+export const ProfileInfosBar = styled.View`
+  margin-left: 10px;
+  
+`;
+
+export const ProfileInfosHorizontal = styled.View`
+  flex-direction: row;
+`;
+
+export const ProfileInfoIcon = styled.View`
+  margin-right: 10px;
+`;
+
+export const ProfileInfosVertical = styled.View`
+  margin-bottom: 10px;
+`;
+
+
+export const ProfileInfoTitle = styled.Text`
+  margin-top: -2px;
+  font-weight: bold;
+`;
+
+
+export const ProfileInfoText = styled.TextInput`
+`;
+
+export const UpdateButton = styled.TouchableOpacity`
+`;
+
+export const UpdateButtonText = styled.Text`
+
+`;
+
+
+export const BorderInput = styled.View`
+    border: 0.5px;
+    margin-top: 5px;
+    border-color: #FFF246;
+`;
+
+export const AddServicesButton = styled.TouchableOpacity`
+  align-self: center;
+  margin-top: 10px;
+  background-color: #FFF246;
+  padding: 8px;
+  border-radius: 6px;
+`;
+
+
+export const AddServicesButtonText = styled.Text`
+  font-weight: bold;
+`;
+
+export const ServicesBar = styled.View`
+  margin-left: 10px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+
+export const ServicesTitle = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const ServicesItemsBar = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+})`
+  
+`;
+
+export const ServicesItemsBarHorizontal = styled.View`
+  
+`;
+
+
+export const ServiceImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  margin-top: 5px;
+`;
+
+export const ServicesItemsBarVertical = styled.View`
+  margin-right: 25px;
+`;
+
+
+export const ServiceTitle = styled.Text`
+  font-weight: bold;
+`;
+
+export const ServiceDescription = styled.Text`
+  
+`;
+
+
+
+
+
+
